757f500fcf796f358fea9d43c0dc4032ae7c43ff
ewalaa496-wq <ewalaa496@gmail.com>
2026-02-21 01:48:29 +0200

    website
    job

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('startdream').onclick =
+function(){
+    document.getElementById('welcome-page').style.display ='none';
+    document.querySelector('.rad').style.display ='none';
+    document.getElementById('dream-page').style.display ='block';
+
+};
